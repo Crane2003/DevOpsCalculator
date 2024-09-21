4cb51ef8ad90b7f8828291eadab59a8d6e67ee10
zuirseime <vovakibets88@gmail.com>
2024-09-21 18:40:18 +0300

    Created a controler and view for the calculator

---
+++ DevOpsCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using DevOpsCalculator.Calculator;
+using DevOpsCalculator.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace DevOpsCalculator.Controllers;
+public class CalculatorController(ICalculator calculator) : Controller
+{
+    [Route("/calculator")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("/calculator")]
+    public IActionResult Calculate(double number1, double number2, string operation)
+    {
+        var methods = typeof(ICalculator).GetMethods(BindingFlags.Public | BindingFlags.Instance);
+        var method = methods.FirstOrDefault(m
+            => m.GetCustomAttribute<OperationAttribute>()?.Operation == operation)
+            ?? throw new ArgumentException("Unknown operation");
+
+        ViewBag.Result = method.Invoke(calculator, [number1, number2]);
+
+        return View("Index");
+    }
+}
