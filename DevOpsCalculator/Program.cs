3bccdcf2cd15b1adfe3b0f867816a4a8a4ebec16
zuirseime <vovakibets88@gmail.com>
2024-09-21 18:39:07 +0300

    Turned on the calculator service in the Program class

--- DevOpsCalculator/Program.cs
+++ DevOpsCalculator/Program.cs
@@ -1,7 +1,12 @@
+using DevOpsCalculator.Calculator;
+using DevOpsCalculator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ICalculator, CalculatorService>();
 
 var app = builder.Build();
 
