191056df4b71653b62ffc4ff1ad6d8b674e89e43
Crane2003 <y.v.miasnykov@student.khai.edu>
2024-09-21 15:58:47 +0300

    Add project files.

---
+++ DevOpsCalculator/Calculator/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace DevOpsCalculator.Calculator;
+
+public interface ICalculator
+{
+    [Operation("add")]
+    double Add(double a, double b);
+    
+    [Operation("substract")]
+    double Substract(double a, double b);
+    
+    [Operation("multiply")]
+    double Multiply(double a, double b);
+    
+    [Operation("divide")]
+    double Divide(double a, double b);
+}
---
+++ DevOpsCalculator/Calculator/OperationAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace DevOpsCalculator.Calculator;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class OperationAttribute(string operation) : Attribute
+{
+    public string Operation { get; set; } = operation;
+}
