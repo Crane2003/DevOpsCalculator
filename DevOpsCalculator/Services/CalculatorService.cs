36b7ada07292b3a10e68d7f150739d4eb7b2df01
zuirseime <vovakibets88@gmail.com>
2024-09-21 18:38:07 +0300

    Created a calculator service

---
+++ DevOpsCalculator/Services/CalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using DevOpsCalculator.Calculator;
+
+namespace DevOpsCalculator.Services;
+
+public class CalculatorService : ICalculator
+{
+    double ICalculator.Add(double a, double b) => a + b;
+    double ICalculator.Subtract(double a, double b) => a - b;
+    double ICalculator.Multiply(double a, double b) => a * b;
+
+    double ICalculator.Divide(double a, double b)
+    {
+        if (b == 0) throw new DivideByZeroException("Division by zero is not allowed.");
+        return a / b;
+    }
+
+}
